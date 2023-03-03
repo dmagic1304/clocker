8402ef1bd601783c4145a18ca43e76caebf027cd
Dominik Magic <dominikmagic5@gmail.com>
2023-03-03 15:44:32 -0800

    it will display the work hour data for clockin/clockout pairs

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,41 +1,40 @@
 import React, { useState, useEffect } from 'react'
 import { db } from './../firebase'
-import { collection, query, where, onSnapshot } from 'firebase/firestore';
+import { collection, query, where, onSnapshot, getDocs } from 'firebase/firestore';
 
 export default function Calendar() {
   const [timestamps, setTimestamps] = useState([]);
   const [error, setError] = useState(null);
 
-
+  const timesQuery = async () => {
+    const times = [];
+    const collectionRef = collection(db, "user");
+    const q = query(collectionRef, where("ClockOut", "!=", null));
+    const snapshot = await getDocs(q);
+    snapshot.forEach(doc => {
+      times.push({
+        ClockIn: doc.data().ClockIn.toDate(),
+        ClockOut: doc.data().ClockOut.toDate()
+      });
+    });
+    console.log('times st ' + timestamps)
+    setTimestamps(times);
+  }
 
   useEffect(() => {
-    const unSubscribe = onSnapshot(
-      query(collection(db, "user"), where("ClockOut", "!=", null)),
-      (collectionSnapshot) => {
-        const times = [];
-        collectionSnapshot.forEach((doc) => {
-          times.push({
-            ClockIn: doc.data().ClockIn,
-            ClockOut: doc.data().ClockOut
-          });
-        });
-        setTimestamps(times);
-      },
-      (error) => {
-        setError(error.message);
-      }
-    );
-    return () => unSubscribe();
+    timesQuery()
   }, []);
+
+
 
   return (
     <div>
-      {timestamps.forEach((stamp) => {
-        <div>
-          <p>{stamp.ClockIn}</p>
-          <p>{stamp.ClockOut}</p>
+      {timestamps.map((stamp, index) => (
+        <div key={index}>
+          <p>{stamp.ClockIn.toString()}</p>
+          <p>{stamp.ClockOut.toString()}</p>
         </div>
-      })}
+      ))}
     </div>
   )
 }
