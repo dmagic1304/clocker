2992b8d4af1cfda78f2bc25dfd430fdcf256c111
Dominik Magic <dominikmagic5@gmail.com>
2023-03-03 09:59:03 -0800

    able to save data to firestore, now implementing clock out ability

--- src/components/ClockIn.js
+++ src/components/ClockIn.js
@@ -5,25 +5,30 @@ import { addDoc, collection } from 'firebase/firestore';
 
 export default function ClockIn() {
   const [user, setUser] = useState(null);
-  const date = format(new Date(), 'Pp');
+  const [clockInTime, setClockInTime] = useState(null);
+  const [clockOutTime, setClockOutTime] = useState(null);
 
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      if (user) {
-        setUser(user);
-      } else {
-        setUser(null);
-      }
-    });
 
-    return () => unsubscribe();
-  }, []);
+  const clockInTime = format(new Date(), 'Pp');
 
-  let handleClockIn = async (newClockIn) => {
+  // useEffect(() => {
+  //   const unsubscribe = auth.onAuthStateChanged((user) => {
+  //     if (user) {
+  //       setUser(user);
+  //     } else {
+  //       setUser(null);
+  //     }
+  //   });
+
+  //   return () => unsubscribe();
+  // }, []);
+
+  const handleClockIn = async (newClockIn) => {
     const collectionRef = collection(db, "user");
     await addDoc(collectionRef, newClockIn);
     console.log("test");
   };
+
 
   // if (!user) {
   //   return <div>Loading...</div>;
