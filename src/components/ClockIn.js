a14a9c2f51ec381e1776a99cff597b58b3f56b8e
Dominik Magic <dominikmagic5@gmail.com>
2023-03-03 09:31:27 -0800

    WIP: still having issues with accessing and saving new data to the firestore

--- src/components/ClockIn.js
+++ src/components/ClockIn.js
@@ -1,21 +1,39 @@
-import React from 'react'
-import { format } from 'date-fns'
-import db from './../firebase.js';
+import React, { useState, useEffect } from 'react';
+import { format } from 'date-fns';
+import { db, auth } from './../firebase';
 import { addDoc, collection } from 'firebase/firestore';
 
-
 export default function ClockIn() {
-  const date = format(new Date(), 'Pp')
+  const [user, setUser] = useState(null);
+  const date = format(new Date(), 'Pp');
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   let handleClockIn = async (newClockIn) => {
     const collectionRef = collection(db, "user");
     await addDoc(collectionRef, newClockIn);
-  }
+    console.log("test");
+  };
+
+  // if (!user) {
+  //   return <div>Loading...</div>;
+  // }
 
   return (
     <div>
-      Welcome "name"  {/*get user name from authorization */}
+      Welcome !
       {date}
-      <button onSubmit={handleClockIn({ date: date })}>Clock in</button>
+      <button onClick={() => handleClockIn({ test: "test data" })}>Clock in</button>
     </div>
-  )
+  );
 }
