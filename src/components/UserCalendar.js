284089f7cef0d9326d816f2d5852988966e244cb
Dominik Magic <dominikmagic5@gmail.com>
2023-03-03 17:11:06 -0800

    it will display clockin data in a basic calendar, need to research additional styling

--- src/components/UserCalendar.js
+++ src/components/UserCalendar.js
@@ -5,33 +5,59 @@ import { db } from './../firebase';
 import { collection, query, where, getDocs } from 'firebase/firestore';
 
 
+
 export default function UserCalendar() {
   const [timestamps, setTimestamps] = useState([]);
   const [error, setError] = useState(null);
+  const [date, setDate] = useState(new Date());
 
   const timesQuery = async () => {
     const times = [];
-    const collectionRef = collection(db, "user");
-    const q = query(collectionRef, where("ClockOut", "!=", null));
+    const collectionRef = collection(db, 'user');
+    const q = query(collectionRef, where('ClockOut', '!=', null));
     const snapshot = await getDocs(q);
-    snapshot.forEach(doc => {
+    snapshot.forEach((doc) => {
       times.push({
         ClockIn: doc.data().ClockIn.toDate(),
-        ClockOut: doc.data().ClockOut.toDate()
+        ClockOut: doc.data().ClockOut.toDate(),
       });
     });
     setTimestamps(times);
-  }
+  };
 
   useEffect(() => {
-    timesQuery()
+    timesQuery();
   }, []);
 
-
+  const tileContent = ({ date, view }) => {
+    if (view === 'month') {
+      const day = date.getDate();
+      const month = date.getMonth();
+      const year = date.getFullYear();
+      const matchingTimestamps = timestamps.filter((timestamp) => {
+        const timestampDate = timestamp.ClockIn.getDate();
+        const timestampMonth = timestamp.ClockIn.getMonth();
+        const timestampYear = timestamp.ClockIn.getFullYear();
+        return day === timestampDate && month === timestampMonth && year === timestampYear;
+      });
+      return (
+        <div>
+          {matchingTimestamps.map((timestamp, index) => (
+            <div key={index}>
+              <p>Clock In: {timestamp.ClockIn.toLocaleString([], { hour: '2-digit', minute: '2-digit' })}</p>
+              <p>Clock Out: {timestamp.ClockOut.toLocaleString([], { hour: '2-digit', minute: '2-digit' })}</p>
+            </div>
+          ))}
+        </div>
+      );
+    } else {
+      return null;
+    }
+  };
 
   return (
     <div>
-      <Calendar />
+      <Calendar value={date} onChange={setDate} tileContent={tileContent} />
     </div>
-  )
+  );
 }
