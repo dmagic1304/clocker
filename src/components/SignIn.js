9a520e016d5f4e628f636fd53c89c197df36798f
Dominik Magic <dominikmagic5@gmail.com>
2023-03-07 16:00:17 -0800

    added basic authentication

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,24 +1,71 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useState } from "react";
+import { auth } from "./../firebase.js";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 
-export default function SignIn() {
+function SignIn() {
+  const [signUpSuccess, setSignUpSuccess] = useState(null);
+  const [signInSuccess, setSignInSuccess] = useState(null);
+
+
+  function doSignUp(event) {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // New code below!
+        setSignUpSuccess(`You've successfully signed up, ${userCredential.user.email}!`)
+      })
+      .catch((error) => {
+        // New code below!
+        setSignUpSuccess(`There was an error signing up: ${error.message}!`)
+      });
+  }
+
+  function doSignIn(event) {
+    event.preventDefault();
+    const email = event.target.signinEmail.value;
+    const password = event.target.signinPassword.value;
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        setSignInSuccess(`You've successfully signed in as ${userCredential.user.email}!`)
+      })
+      .catch((error) => {
+        setSignInSuccess(`There was an error signing in: ${error.message}!`)
+      });
+  }
+
   return (
-    <div>
-      <h2>Sign In</h2>
-      <form>
-        <div>
-          <label>Username </label>
-          <input type="text" name="name" required />
-        </div>
-        <div className="input-container">
-          <label>Password </label>
-          <input type="password" name="pass" required />
-        </div>
-        <div>
-          <input type="submit" />
-        </div>
+    <React.Fragment>
+      <h1>Sign up</h1>
+      {signUpSuccess}
+      <form onSubmit={doSignUp}>
+        <input
+          type='text'
+          name='email'
+          placeholder='email' />
+        <input
+          type='password'
+          name='password'
+          placeholder='Password' />
+        <button type='submit'>Sign up</button>
       </form>
-    </div>
-  )
+      <h1>Sign In</h1>
+      {signInSuccess}
+      <form onSubmit={doSignIn}>
+        <input
+          type='text'
+          name='signinEmail'
+          placeholder='email' />
+        <input
+          type='password'
+          name='signinPassword'
+          placeholder='Password' />
+        <button type='submit'>Sign in</button>
+      </form>
+    </React.Fragment>
+  );
 }
+
+export default SignIn
