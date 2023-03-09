e611d8ec6824e64236b66fa4a5319096d2b2e264
Dominik Magic <dominikmagic5@gmail.com>
2023-03-09 06:50:30 -0800

    navigation moved to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,40 @@
-import { Box, Grid } from '@mui/material'
-import React from 'react'
-import Navigation from './Navigation'
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "./../theme";
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import LoginOutlinedIcon from '@mui/icons-material/LoginOutlined';
 import logo from '../assets/logo.png'
+import { NavLink } from "react-router-dom";
 
-export default function Header() {
+const Header = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+
   return (
-    <Box>
-      <Grid container>
-        <Grid item>
-          <img src={logo} alt="logo" />
-        </Grid >
-        <Grid item>
-          <Navigation />
-        </Grid>
-      </Grid>
+    <Box display="flex" justifyContent="space-between" p={2} border={"solid"}>
+      {/* SEARCH BAR */}
+      <Box
+        display="flex"
+      >
+        <img src={logo} alt="logo" height={100} />
+      </Box>
+
+      {/* ICONS */}
+      <Box display="flex">
+        <IconButton>
+          <NavLink to='/'>
+            <HomeOutlinedIcon />
+          </NavLink>
+        </IconButton>
+        <IconButton>
+          <NavLink to='/SignIn'>
+            <LoginOutlinedIcon />
+          </NavLink>
+        </IconButton>
+      </Box>
     </Box>
-  )
-}
+  );
+};
+
+export default Header;
