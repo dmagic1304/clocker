3ec265fd1cbf896114665d8668a52ff300404710
Dominik Magic <dominikmagic5@gmail.com>
2023-03-09 06:19:26 -0800

    added a logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,19 @@
+import { Box, Grid } from '@mui/material'
 import React from 'react'
 import Navigation from './Navigation'
+import logo from '../assets/logo.png'
 
 export default function Header() {
   return (
-    <div>
-      <h1>Clocker</h1>
-      <Navigation />
-    </div>
+    <Box>
+      <Grid container>
+        <Grid item>
+          <img src={logo} alt="logo" />
+        </Grid >
+        <Grid item>
+          <Navigation />
+        </Grid>
+      </Grid>
+    </Box>
   )
 }
